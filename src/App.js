33cad39dd7e05810e09c450c26f16598614f53bd
Anna <114008959+AnnaSolovykh@users.noreply.github.com>
2023-03-04 15:20:52 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import './App.css';
+import { MyMeals } from './MyMeals';
+import { useEffect, useState } from 'react';
+import { getAllMeals, addMeal, editMeal, deleteMeal } from './FetchMeals';
+
+function App() {
+
+  const [myMeal, setMeal] = useState([]);
+  const [title, setTitle] = useState("");
+  const [editing, setEditing] = useState(false);
+  const [mealId, setMealId] = useState("");
+//то есть нужно, чтобы при нажатии editing можно было менять текст в input
+  useEffect( () => {
+    getAllMeals(setMeal)
+  }, [])
+
+  const updatingInInput = (_id, title) => {
+    //для это нужны id элемента и его title, который мы будем менять
+    setEditing(true)
+    setTitle(title)
+    setMealId(_id)
+  }
+
+  return (
+    <div>
+      <h1>Meal Plan</h1>
+      <input 
+      type="text"
+      placeholder="Add a meal"
+      value={title}
+      onChange={(e) => setTitle (e.target.value)}//чтобы  печаталось, setTitle - потому что меняем состояние
+      />
+
+      <button 
+      disabled={!title}
+      onClick=
+        { editing ? () => editMeal(mealId, title, setTitle, setMeal, setEditing) : 
+        () => addMeal(title, setTitle, setMeal)}>
+        {editing ? "Edit" : "Add"}
+      </button>
+
+      {myMeal.map((meal) => 
+      <MyMeals key={meal._id} text={meal.title} 
+      updatingInInput={()=> updatingInInput(meal._id, meal.title)}
+      deleteMeal={ () => deleteMeal(meal._id, setMeal) } 
+      //каждое блюда просматриваем через app.js и удаляем по id
+      />)}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FetchMeals.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+const getAllMeals = (setMeal) => {
+    axios.get("https://meals-planner.onrender.com")
+    .then (({data}) => {console.log(data)
+    setMeal(data)
+    })
+}
+
+const addMeal = (title, setTitle, setMeal) => {
+    axios.post("https://meals-planner.onrender.com/saveMeals", {title}) //because there is saveMeals in router
+    .then ((data) => {
+        console.log(data)
+        setTitle("")
+        getAllMeals(setMeal)
+    })
+}
+
+const editMeal = (mealId, title, setTitle, setMeal, setEditing) => { //порядок здесь должен совпадать с порядком в fetchMeals
+    //mealId-  id конкретного \лемента, который мы доабвляем , 
+    //title - потому что работаем с текстом, setTitle -  потомку что нужна возможность менять текст,
+    // setMeal - потому что меняем меню , setEditing - потому что хотим редактировать меню
+    axios.put("https://meals-planner.onrender.com/editMeal", {_id: mealId, title}) //because there is saveMeals in router
+    .then ((data) => {
+        console.log(data)
+        setTitle("")
+        setEditing(false) //чтобы появилась возможность менять только при клики на иконку edit
+        getAllMeals(setMeal)
+    })
+}
+
+const deleteMeal = (mealId, setMeal) => { //вводим принцип, по которому удаляем
+    //то есть удаляем по _id, а setMeal нужен, что потом показать остальные
+    axios.post("https://meals-planner.onrender.com/deleteMeal", {_id: mealId }) //because there is saveMeals in router
+    .then ((data) => {
+        console.log(data)
+        getAllMeals(setMeal)//показываем элементы меню
+    })
+}
+
+export { getAllMeals, addMeal, editMeal, deleteMeal }
